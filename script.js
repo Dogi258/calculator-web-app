765c8789c2e1d19fc8307a7a872eaeb3a8f402a9
Daniel <mtz.daniel21@gmail.com>
2021-10-25 01:30:39 -0500

    basic math functions complete

---
+++ script.js
@@ -0,0 +1,34 @@
+/* Basic arithmatic funcitons */
+
+function add(a, b) {
+  return a + b;
+}
+function subtract(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+/*  Takes an operater and 2 numbers, then returns the result of the 
+    appropriate operation */
+function operate(operater, a, b) {
+  if (operater == "+") {
+    return add(a, b);
+  } else if (operater == "-") {
+    return subtract(a, b);
+  } else if (operater == "x") {
+    return multiply(a, b);
+  } else {
+    return divide(a, b);
+  }
+}
+
+console.log(operate("+", 4, 3));
+console.log(operate("-", 4, 3));
+console.log(operate("x", 4, 3));
+console.log(operate("/", 4, 3));
