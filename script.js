9686180c3869f27e2d29dae072bc9f4d4ec3efc8
Daniel <mtz.daniel21@gmail.com>
2021-10-25 03:45:57 -0500

    added functionality to number buttons and clear button

--- script.js
+++ script.js
@@ -28,6 +28,31 @@ function operate(operater, a, b) {
   }
 }
 
+/* Adds number from the argument to the display div */
+function addToDisplay(e) {
+  console.log(e.target.textContent);
+  console.log((displayDiv.textContent += e.target.textContent));
+}
+
+function clearDisplay() {
+  displayDiv.textContent = "";
+}
+
+// Declare display number variable
+let displayNumber = 0;
+// Get display div
+const displayDiv = document.querySelector(".display");
+// Get all number buttons
+const numberButtons = document.querySelectorAll(".number");
+
+// Add event listeners to numbers to add to the display
+numberButtons.forEach((number) =>
+  number.addEventListener("click", addToDisplay)
+);
+
+// Add clear functionality to the clear button
+document.querySelector(".btn-clear").addEventListener("click", clearDisplay);
+
 console.log(operate("+", 4, 3));
 console.log(operate("-", 4, 3));
 console.log(operate("x", 4, 3));
